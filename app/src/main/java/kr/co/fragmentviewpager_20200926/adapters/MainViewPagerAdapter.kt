2a1d841ed8b48c0609eb6de302606bd26f206b46
zizi0302 <50587019+zizi0302@users.noreply.github.com>
2020-09-27 06:39:28 +0900

    프래그먼트를 뿌려주는 뷰페이저 완성

---
+++ app/src/main/java/kr/co/fragmentviewpager_20200926/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package kr.co.fragmentviewpager_20200926.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kr.co.fragmentviewpager_20200926.fragments.MyAddressFragment
+import kr.co.fragmentviewpager_20200926.fragments.MyAgeFragment
+import kr.co.fragmentviewpager_20200926.fragments.MyNameFragment
+
+class MainViewPagerAdapter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+
+        if(position == 0) {
+            return MyNameFragment()
+        } else if (position == 1) {
+            return MyAgeFragment()
+        } else {
+            return MyAddressFragment()
+        }
+    }
+
+
+    override fun getCount(): Int {
+        return 3
+
+    }
+}
