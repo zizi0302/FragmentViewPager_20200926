cab866d707e8ff1bfa46960bc35c444bfe254141
zizi0302 <50587019+zizi0302@users.noreply.github.com>
2020-09-27 06:31:50 +0900

    이름, 나이 , 거주지 각각 프래그먼트로 표시

---
+++ app/src/main/java/kr/co/fragmentviewpager_20200926/fragments/MyAddressFragment.kt
@@ -0,0 +1,19 @@
+package kr.co.fragmentviewpager_20200926.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.fragmentviewpager_20200926.R
+
+class MyAddressFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_address, container, false)
+    }
+}
